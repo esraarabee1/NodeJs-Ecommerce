949de177ad51e38dff917b5786b8837ebe83d040
esraarabee1 <esraarabee69@gmail.com>
2026-05-15 11:54:04 -0700

    Add To Wishlist -Get Logged User Wishlist - Remove From Wishlist

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const productRoute = require("./routes/productRoute");
 const userRoute = require("./routes/userRoute");
 const authRoute = require("./routes/authRoute");
 const reviewRoute = require("./routes/reviewRoute");
+const wishlistRoute = require("./routes/wishlistRoute");
 //connect with db
 dbConnection();
 
@@ -35,6 +36,7 @@ app.use("/api/v1/products", productRoute);
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/reviews", reviewRoute);
+app.use("/api/v1/wishlist", wishlistRoute);
 app.all("*", (req, res, next) => {
   next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
 });
